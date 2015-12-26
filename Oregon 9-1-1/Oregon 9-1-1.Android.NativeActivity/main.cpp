b21e8c1b3a3e2270ef38bd455a888b0218dd9bad
Brandan Lasley <brandan_lasley@hotmail.com>
2015-12-25 18:49:48 -0800

    Call Info

--- Oregon 9-1-1/Oregon 9-1-1.Android.NativeActivity/main.cpp
+++ Oregon 9-1-1/Oregon 9-1-1.Android.NativeActivity/main.cpp
@@ -1,3 +1,5 @@
+#include "Oregon911.h"
+
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "Oregon 9-1-1.NativeActivity", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "Oregon 9-1-1.NativeActivity", __VA_ARGS__))
 
