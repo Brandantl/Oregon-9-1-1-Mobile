5a9b42b9729972eb7564e052dbc3b213b784cb0f
Brandan Tyler Lasley <admin@brandanlasley.com>
2015-12-30 16:48:44 -0800

    OnClick function for call.

--- Oregon9-1-1/app/src/main/java/net/oregon91/cad/oregon9_1_1/MainActivity.java
+++ Oregon9-1-1/app/src/main/java/net/oregon91/cad/oregon9_1_1/MainActivity.java
@@ -1,15 +1,19 @@
 package net.oregon91.cad.oregon9_1_1;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-
 import cad.oregon911.net.Incident;
 import cad.oregon911.net.Oregon911;
 import cad.oregon911.net.callinfo;
 import cad.oregon911.net.timestamp;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Oregon911 OR911;
@@ -26,7 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         OR911.setContext(this);
         calllist = (ListView) findViewById(R.id.CallListView);
+
+        calllist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                parent.findViewById(R.id.call_text);
+                TextView calltxt = (TextView)parent.findViewById(R.id.call_text);
+                Toast.makeText(MainActivity.this, calltxt.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
         refreshButton(null);
+
+
     }
 
     @Override
